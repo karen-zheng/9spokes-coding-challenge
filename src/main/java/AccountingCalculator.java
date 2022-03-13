06abfaab831671502eca996bccd06a2de55e7997
karen <karen@applicable.co.nz>
2022-03-13 14:49:33 +1300

    Feat: Setup project

---
+++ src/main/java/AccountingCalculator.java
@@ -0,0 +1,22 @@
+public class AccountingCalculator {
+    public static void main(String[] args) {
+        Object
+    }
+    //Read file
+
+    //Return results
+
+    //Calculate revenue
+
+    //Calculate expense
+
+    //Calculate Profit marging
+
+    //Calculate Gross rofit
+
+    //Calculate Net Profit
+
+    //Calculate Working Capital Ratio
+
+    //Formating
+}
---
+++ src/main/java/EntryModel.java
@@ -0,0 +1,13 @@
+public class EntryModel {
+    private String account_category;
+    private String account_code;
+    private String account_currency;
+    private String account_identifier;
+    private String account_status;
+    private String value_type;
+    private String account_name;
+    private String account_type;
+    private String account_type_bank;
+    private String system_account;
+    private Float total_value;
+}
